498d68d16d3561e578d467c1b959e952760139b0
luisescx <luis.esculapio@Live.com>
2022-01-09 17:17:48 -0300

    add product to firestore

--- src/components/FormBox/index.tsx
+++ src/components/FormBox/index.tsx
@@ -1,29 +1,84 @@
-import React from 'react';
-
-import { Container } from './styles';
-import { ButtonIcon } from '../ButtonIcon';
-import { Input } from '../Input';
+import React, { useState, useCallback } from "react";
+import firestore from "@react-native-firebase/firestore";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { Container } from "./styles";
+import { ButtonIcon } from "../ButtonIcon";
+import { Input } from "../Input";
+import { Alert, Keyboard } from "react-native";
 
 export function FormBox() {
-  return (
-    <Container>
-      <Input
-        placeholder="Nome do produto"
-        size="medium"
-      />
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(0);
 
-      <Input
-        placeholder="0"
-        keyboardType="numeric"
-        size="small"
-        style={{ marginHorizontal: 8 }}
-      />
+    const navigation = useNavigation();
 
-      <ButtonIcon
-        size='large'
-        icon="add-shopping-cart"
-        onPress={() => { }}
-      />
-    </Container>
-  );
+    async function handleProductAdd() {
+        // to add a custom id, as uuid or a number
+        // .doc("my-custom-id")
+        // .set
+
+        firestore()
+            .collection("products")
+            .add({
+                description,
+                quantity,
+                done: false,
+                createdAt: firestore.FieldValue.serverTimestamp(),
+            })
+            .then(() => {
+                Alert.alert("Produto adicionado com sucesso");
+
+                setDescription("");
+                setQuantity(0);
+            })
+            .catch((error) => {
+                console.log(error);
+                Alert.alert(
+                    "Erro ao adicionar produto, tente novamente mais tarde"
+                );
+
+                setDescription("");
+                setQuantity(0);
+            });
+
+        Keyboard.dismiss();
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+            const unsubscribe = navigation.addListener("blur", () => {
+                setDescription("");
+                setQuantity(0);
+            });
+
+            return unsubscribe;
+        }, [])
+    );
+
+    return (
+        <Container>
+            <Input
+                placeholder="Nome do produto"
+                size="medium"
+                onChangeText={setDescription}
+                value={description}
+                autoCapitalize="sentences"
+            />
+
+            <Input
+                placeholder="0"
+                keyboardType="numeric"
+                size="small"
+                style={{ marginHorizontal: 8 }}
+                onChangeText={(value) => setQuantity(Number(value))}
+                value={String(quantity)}
+            />
+
+            <ButtonIcon
+                size="large"
+                icon="add-shopping-cart"
+                onPress={handleProductAdd}
+            />
+        </Container>
+    );
 }
--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,22 +1,56 @@
-import React, { useState } from 'react';
-import { FlatList } from 'react-native';
-
-import { styles } from './styles';
-import { Product, ProductProps } from '../Product';
-
-import { shoppingListExample } from '../../utils/shopping.list.data';
+import React, { useEffect, useState } from "react";
+import { FlatList } from "react-native";
+import firestore from "@react-native-firebase/firestore";
+import { styles } from "./styles";
+import { Product, ProductProps } from "../Product";
 
 export function ShoppingList() {
-  const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
+    const [products, setProducts] = useState<ProductProps[]>([]);
 
-  return (
-    <FlatList
-      data={products}
-      keyExtractor={item => item.id}
-      renderItem={({ item }) => <Product data={item} />}
-      showsVerticalScrollIndicator={false}
-      style={styles.list}
-      contentContainerStyle={styles.content}
-    />
-  );
+    // useEffect(() => {
+    //     firestore()
+    //         .collection("products")
+    //         .doc("string-id")
+    //         .get()
+    //         .then((response) =>
+    //             console.log({
+    //                 id: response.id,
+    //                 ...response.data(),
+    //             })
+    //         );
+    // }, []);
+
+    useEffect(() => {
+        const subscribe = firestore()
+            .collection("products")
+            .orderBy("createdAt", "desc")
+            .onSnapshot(
+                (querySnapshot) => {
+                    const data = querySnapshot.docs.map((doc) => {
+                        return {
+                            id: doc.id,
+                            ...doc.data(),
+                        };
+                    }) as ProductProps[];
+
+                    setProducts(data);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+
+        return () => subscribe();
+    }, []);
+
+    return (
+        <FlatList
+            data={products}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <Product data={item} />}
+            showsVerticalScrollIndicator={false}
+            style={styles.list}
+            contentContainerStyle={styles.content}
+        />
+    );
 }
