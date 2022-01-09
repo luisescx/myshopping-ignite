b92d4a0a830802c21c0586ba0b12065d8d41dd3f
luisescx <luis.esculapio@Live.com>
2022-01-09 20:46:51 -0300

    add login auth

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import { ButtonIcon } from '../ButtonIcon';
-
-import { Container, Title } from './styles';
+import React from "react";
+import { ButtonIcon } from "../ButtonIcon";
+import auth from "@react-native-firebase/auth";
+import { Container, Title } from "./styles";
 
 type Props = {
-  title: string;
-  showLogoutButton?: boolean;
-}
+    title: string;
+    showLogoutButton?: boolean;
+};
 
 export function Header({ title, showLogoutButton = false }: Props) {
-  return (
-    <Container showLogoutButton={showLogoutButton}>
-      <Title>
-        {title}
-      </Title>
+    function handleLogout() {
+        auth().signOut();
+    }
 
-      {
-        showLogoutButton &&
-        <ButtonIcon
-          icon="logout"
-          color="alert"
-          style={{ marginTop: 20 }}
-          onPress={() => {}}
-        />
-      }
-    </Container>
-  );
+    return (
+        <Container showLogoutButton={showLogoutButton}>
+            <Title>{title}</Title>
+
+            {showLogoutButton && (
+                <ButtonIcon
+                    icon="logout"
+                    color="alert"
+                    style={{ marginTop: 20 }}
+                    onPress={handleLogout}
+                />
+            )}
+        </Container>
+    );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,32 +1,90 @@
-import React from 'react';
-
-import { Container, Account, Title, Subtitle } from './styles';
-import { ButtonText } from '../../components/ButtonText';
-import { Button } from '../../components/Button';
-import { Input } from '../../components/Input';
+import React, { useState } from "react";
+import auth from "@react-native-firebase/auth";
+import { Container, Account, Title, Subtitle } from "./styles";
+import { ButtonText } from "../../components/ButtonText";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+import { Alert } from "react-native";
 
 export function SignIn() {
-  return (
-    <Container>
-      <Title>MyShopping</Title>
-      <Subtitle>monte sua lista de compra te ajudar nas compras</Subtitle>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-      <Input
-        placeholder="e-mail"
-        keyboardType="email-address"
-      />
+    async function handleSignIn() {
+        auth()
+            .signInWithEmailAndPassword(email, password)
+            .catch((error) => {
+                if (
+                    error.code === "auth/user-not-found" ||
+                    error.code === "auth/wrong-password"
+                ) {
+                    return Alert.alert(
+                        "Usuário não encontrado. Email e/ou senha inválida"
+                    );
+                }
+            });
+    }
 
-      <Input
-        placeholder="senha"
-        secureTextEntry
-      />
+    async function handleForgotPassword() {
+        auth()
+            .sendPasswordResetEmail(email)
+            .then(() =>
+                Alert.alert(
+                    "Enviamos um link no seu e-mail para você redefinir sua senha"
+                )
+            );
+    }
 
-      <Button title="Entrar" onPress={() => { }} />
+    function handleCreateUserAccount() {
+        auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(() => Alert.alert("Usúario criado com sucesso!"))
+            .catch((error) => {
+                if (error.code === "auth/email-already-in-use") {
+                    return Alert.alert(
+                        "E-mail não disponível. Escolha outro e-mail para cadastrar!"
+                    );
+                }
 
-      <Account>
-        <ButtonText title="Recuperar senha" onPress={() => { }} />
-        <ButtonText title="Criar minha conta" onPress={() => { }} />
-      </Account>
-    </Container>
-  );
+                if (error.code === "auth/invalid-email") {
+                    return Alert.alert("E-mail inválido");
+                }
+
+                if (error.code === "auth/weak-password") {
+                    return Alert.alert("A senha deve ter no mínimo 6 dígitos");
+                }
+            });
+    }
+
+    return (
+        <Container>
+            <Title>MyShopping</Title>
+            <Subtitle>monte sua lista de compra te ajudar nas compras</Subtitle>
+
+            <Input
+                placeholder="e-mail"
+                keyboardType="email-address"
+                onChangeText={setEmail}
+            />
+
+            <Input
+                placeholder="senha"
+                secureTextEntry
+                onChangeText={setPassword}
+            />
+
+            <Button title="Entrar" onPress={handleSignIn} />
+
+            <Account>
+                <ButtonText
+                    title="Recuperar senha"
+                    onPress={handleForgotPassword}
+                />
+                <ButtonText
+                    title="Criar minha conta"
+                    onPress={handleCreateUserAccount}
+                />
+            </Account>
+        </Container>
+    );
 }
